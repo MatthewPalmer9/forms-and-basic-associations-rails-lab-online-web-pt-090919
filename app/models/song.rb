f5feda1fff68595c04ba01a27baa69f091ee35d5
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-08 04:27:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,7 +11,7 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def genre_name=(name)
+  def genre_name=(genre)
     self.genre = Genre.find_or_create_by(name: name)
   end
 
