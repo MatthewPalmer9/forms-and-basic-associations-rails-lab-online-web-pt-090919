0e823a1b28f294220ddc7ad9555fbb2bd1b1b890
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-08 04:35:20 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,14 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  # add associations here
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
-
-  def artist_name
-    self.artist.name if self.artist
-  end
 
   def genre_name=(genre)
     self.genre = Genre.find_or_create_by(name: genre)
@@ -17,6 +9,14 @@ class Song < ActiveRecord::Base
 
   def genre_name
     self.genre.name if self.genre
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
   def note_contents=(content)
